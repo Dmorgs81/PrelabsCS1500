be9ff6a36af594bc0b3184c9fb90805d56551c20
Dmorgs81 <61892658+Dmorgs81@users.noreply.github.com>
2024-10-17 12:41:01 -0500

    Create Lab2.c

---
+++ Lab2.c
@@ -0,0 +1,27 @@
+// Made by David Morgensztern
+//Date 8/30/24
+//Lab 2
+//CMP_SC 1500
+
+
+#include <stdio.h>
+
+
+int main()
+{
+   int x;
+   int y;
+   
+   printf("What is your x first number: ");
+   scanf("%d", &x);
+   printf("What is your y second number: ");
+   scanf("%d", &y);
+   printf("x = %d\n", x);
+   printf("y = %d\n", y);
+   printf("x / y = %d\n", x / y);
+   printf("y - 2 * x = %d\n", (y - 2) * x);
+   printf("%d", x);
+   printf("%d", y);
+  
+   return 0;
+}
